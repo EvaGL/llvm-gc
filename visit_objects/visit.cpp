01a80054ae4110dd4afe5e2c5b445accff610353
elizabeth <elizabeth.shashkova@gmail.com>
2013-11-27 23:32:42 +0400

    Formatting changes

--- visit_objects/visit.cpp
+++ visit_objects/visit.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 
 void visit_object(void* obj) {
-	if (!obj) {
-		return;
-	}
-	std::cout << obj << std::endl;
-	void* ptr = obj;
-	int* num_ptr =  (int*) ptr;
-	int num = *num_ptr;
-	for (int i = 1; i <= num; ++i) {
-		num_ptr++;
-		int* res = (int *) (obj + *num_ptr);
-		visit_object((void*)*res);
-	}
+    if (!obj) {
+        return;
+    }
+    std::cout << obj << std::endl;
+    void* ptr = obj;
+    int* num_ptr =  (int*) ptr;
+    int num = *num_ptr;
+    for (int i = 1; i <= num; ++i) {
+        num_ptr++;
+        int* res = (int *) (obj + *num_ptr);
+        visit_object((void*)*res);
+    }
 
 }
